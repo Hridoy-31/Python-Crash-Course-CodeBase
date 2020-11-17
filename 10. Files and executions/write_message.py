2a105fdc9550893d122f63603e0b0353daab017b
Hridoy-31 <raihanulbashirhridoy@gmail.com>
2020-11-17 23:54:24 +0600

    Initial commit

---
+++ 10. Files and executions/write_message.py
@@ -0,0 +1,6 @@
+filename = '/home/hridoy/github/Python-Crash-Course-CodeBase/10. Files and executions/programming.txt'
+
+with open(filename, 'a') as file_object:
+    file_object.write("I love playing PC games too.\n")
+    file_object.write("I love to use C++ for competitive programming.\n")
+
