75441411869dbf0e8d0d38ef7b1ff472fd09ea83
Hridoy-31 <raihanulbashirhridoy@gmail.com>
2020-11-17 17:42:11 +0600

    initial commit

--- 10. Files and executions/pi_string.py
+++ 10. Files and executions/pi_string.py
@@ -1,4 +1,4 @@
-filename = '/home/hridoy/github/Python-Crash-Course-CodeBase/10. Files and executions/pi_digits.txt'
+filename = '/home/hridoy/github/Python-Crash-Course-CodeBase/10. Files and executions/pi_million_digits.txt'
 
 with open(filename) as file_object:
     lines = file_object.readlines()
@@ -8,5 +8,5 @@ py_str = ''
 for line in lines:
     py_str += line.strip()
 
-print(py_str)
+print(f"{py_str[:52]}...")
 print(len(py_str))
